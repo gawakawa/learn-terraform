0b8c461f487bf6db70a20fb61afec5b95ab2f1b6
gawakawa <mashurakiryu@icloud.com>
2025-04-13 20:38:31 +0900

    :sparkles: Add a VM instance

--- gcp/main.tf
+++ gcp/main.tf
@@ -10,9 +10,27 @@ terraform {
 provider "google" {
   project = "terraform-456709"
   region  = "asia-northeast1"
-  zone    = "asia-notrheast1-a"
+  zone    = "asia-northeast1-a"
 }
 
 resource "google_compute_network" "vpc_network" {
   name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+  tags         = ["web", "dev"]
+
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
 }
